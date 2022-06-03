89289c6d29c35fd0a7b2226d8e6a94cef0796834
sumonahmed125 <masumahmed6763@gmail.com>
2022-06-04 01:43:42 +0600

    first commit

---
+++ NodeJS_CRUD/api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connection Succesfully"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 6000, () => {
+  console.log("server is running on port 6000");
+});
---
+++ NodeJS_CRUD/client/src/App.jsx
@@ -0,0 +1,8 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (<Register></Register>), (<Login></Login>);
+};
+
+export default App;
